5a6d5e9fe94832ff6f366983afd593a7f0736fd6
Nathan Banaszak <56935011+Zubinatub@users.noreply.github.com>
2021-12-13 04:00:17 -0500

    removed changes I used for testing

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const PORT = process.env.PORT||'5000';
 
 module.exports = function (app) {
   app.use(
     ["/api/*"],
     createProxyMiddleware({
-      target: "http://localhost:" + PORT,
+      target: "http://localhost:5000",
     })
   );
 };
